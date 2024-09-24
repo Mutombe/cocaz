0acf92f87448c2aa0b9f3adf78f4bb23e077c0d3
Mutombe <simbarashemutombe1@gmail.com>
2024-09-24 23:10:43 +0200

    hello world

---
+++ cocaz-static/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-red-500'>Hello with Tailwind CSS</h1>
+    </>
+  )
+}
+
+export default App
